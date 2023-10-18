60d271346d77b10539f1d569b27003f4583c13c1
yadik64 <yadik64@gmail.com>
2023-10-18 16:52:18 +0400

    create project

---
+++ NoteRoom/NoteRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteRoomApp.swift
+//  NoteRoom
+//
+//  Created by Дмитрий Яровой on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct NoteRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
